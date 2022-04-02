17682fbfe0b317f884e3ababd81deb1fcb8d811f
Peter Nunn <pnunn@intq.it>
2022-04-02 14:40:47 +1100

    Initial entry

---
+++ handlers/hardlers.go
@@ -0,0 +1,21 @@
+package handlers
+
+import (
+	"github.com/CloudyKit/jet/v6"
+	"github.com/pnunn/celeritas"
+	"myapp/data"
+	"net/http"
+)
+
+type Handlers struct {
+	App    *celeritas.Celeritas
+	Models data.Models
+}
+
+func (h *Handlers) Home(w http.ResponseWriter, r *http.Request) {
+	err := h.render(w, r, "home", nil, nil)
+	if err != nil {
+		h.App.ErrorLog.Println("error rendering:", err)
+	}
+}
+
